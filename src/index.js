a5197f52277b4d3aef39161ea18ef86282c1e2b7
chuck <you@example.com>
2022-04-25 17:05:07 -0700

    Modifying settings modal

--- src/index.js
+++ src/index.js
@@ -1,44 +1,87 @@
-import * as THREE from "three"
-import "./reset.scss"
-import "./index.scss"
+// import * as THREE from "three"
 
-// Create an empty scene
-let scene = new THREE.Scene();
+import Timer from "./timer.js"
 
-// Create a basic perspective camera
-let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 4;
 
-// Create a renderer with Antialiasing
-// let renderer = new THREE.WebGLRenderer({antialias:true});
+let timer = new Timer(.1,'timer-value');
 
-// Configure renderer clear color
-renderer.setClearColor("#000000");
+let hello = document.querySelector(".settings")
+let settingsPage = document.querySelector('.modal')
+let start = document.querySelector(".timer.start")
+let pause = document.querySelector(".timer.pause")
+let reset = document.querySelector(".timer.reset")
+let closeModal = document.querySelector(".modal")
+let close = document.querySelector(".close")
+closeModal.addEventListener("click",function(){
+  closeModal.style.display = "none"
+})
 
-// Configure renderer size
-renderer.setSize( window.innerWidth, window.innerHeight );
+hello.addEventListener("click",function(){
+  let on = settingsPage.style.display
+  if (on === "block") {
+    settingsPage.style.display = "none"
+  } else {
+    settingsPage.style.display = "flex"
+    settingsPage.style.gridTemplateColumns = 1
+    
+  }
+})
 
-// Append Renderer to DOM This is where the canvas is written to so i need to add this as a child of the
-// thing that i want it to be on 
-document.body.appendChild( renderer.domElement );
+start.addEventListener("click", function() {
 
-// Create a Cube Mesh with basic material
-let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-let material = new THREE.MeshBasicMaterial( { color: "#ffffff" } );
-let cube = new THREE.Mesh( geometry, material );
+  timer.startTimer()
+})
 
-// Add cube to Scene
-scene.add(cube);
+pause.addEventListener("click",function(){
+  timer.pause()
+})
 
-// Render Loop
-let render = function () {
-  requestAnimationFrame( render );
+reset.addEventListener("click",function(){
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  timer.resetTimer()
 
-  // Render the scene
-  renderer.render(scene, camera);
-};
+})
+// document.addEventListener("")
 
-render();
+
+
+// // Create an empty scene
+// let scene = new THREE.Scene();
+
+// // Create a basic perspective camera
+// let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// camera.position.z = 4;
+
+// // Create a renderer with Antialiasing
+// // let renderer = new THREE.WebGLRenderer({antialias:true});
+
+// // Configure renderer clear color
+// renderer.setClearColor("#000000");
+
+// // Configure renderer size
+// renderer.setSize( window.innerWidth, window.innerHeight );
+
+// // Append Renderer to DOM This is where the canvas is written to so i need to add this as a child of the
+// // thing that i want it to be on 
+// document.body.appendChild( renderer.domElement );
+
+// // Create a Cube Mesh with basic material
+// let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// let material = new THREE.MeshBasicMaterial( { color: "#ffffff" } );
+// let cube = new THREE.Mesh( geometry, material );
+
+// // Add cube to Scene
+// scene.add(cube);
+
+// // Render Loop
+// let render = function () {
+//   requestAnimationFrame( render );
+
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
+
+//   // Render the scene
+//   renderer.render(scene, camera);
+// };
+
+// render();
---
+++ src/timer.js
@@ -0,0 +1,75 @@
+
+class Timer {
+    constructor(length,htmlElement) {
+        // 1. Take in the length of the timer in minutes and where you want the timer to display
+        // 2. Save the seconds as the length * 60
+        // 3. Split out the minutes and seconds for timer display
+        this.length = length //for the reset of the timer
+        this.seconds = length * 60;
+        this.minutesShow = Math.floor(this.seconds / 60);
+        this.secondsShow = this.seconds % 60
+        this.start = false;
+        this.htmlElement = htmlElement; //id of the where you want the timer to print
+        this.printTime()
+      
+    }
+    
+    startTimer() {
+      this.timerId = setInterval(this._tick.bind(this), 1000);
+      this.start = true;
+    }
+
+    pause() {
+      clearInterval(this.timerId);
+      this.timerId = null;
+      this.start = false;
+    }
+
+    printTime() {
+        // Format the time in MM:SS
+        // Use console.log to print it.
+        // console.clear();
+        // console.log("inside printTime");
+        let timeDisplay = document.querySelector(`.${this.htmlElement}`)
+        // console.log(
+        //   `${this.minutesShow.toString().padStart(2, '0')}:` +
+        //   `${(this.secondsShow).toString().padStart(2, '0')}`);
+        
+        this.minutesShow = Math.floor(this.seconds / 60);
+        this.secondsShow = this.seconds % 60;
+       
+        if (timeDisplay !== null) {
+          timeDisplay.innerHTML =`${this.minutesShow.toString().padStart(2, '0')}:` + `${(this.secondsShow).toString().padStart(2, '0')}`;
+        }
+        
+        // console.log(`the seconds remaining are ${this.seconds}`);
+        
+    }
+        
+    _tick() {
+      if (this.seconds === 0) { 
+        this.pause();
+        this.resetTimer();
+
+      } else {
+        this.seconds -= 1;
+        this.time_remaining = this.seconds;
+
+      }
+      // console.log("calling printTime()")
+      return this.printTime();
+      // 1. Decrement the time by one second.
+      // 2. Store the number of ticks passed
+      // 3. Call printTime.
+    }
+
+    resetTimer(){
+      this.seconds = (this.length) * 60;
+      this.printTime()
+    }
+}
+
+// let timer = new Timer(.1);
+
+export default Timer
+
